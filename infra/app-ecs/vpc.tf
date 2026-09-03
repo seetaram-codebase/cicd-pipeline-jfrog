f6ba029bb50099dc9ba64ed58cc3850f36fa0003
seetaram <seetaram.r@gmail.com>
2026-09-03 02:02:30 -0400

    Fix: AWS SG description doesn't allow em-dash

--- infra/app-ecs/vpc.tf
+++ infra/app-ecs/vpc.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "alb" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "Public HTTP — this is the demo API's public entry point"
+    description = "Public HTTP - this is the demo API's public entry point"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
